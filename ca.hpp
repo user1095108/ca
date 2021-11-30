027e34bacaf6d5d131b2d5f18d924b8150379355
Janez Žemva <janezz55@gmail.com>
2021-11-30 17:20:11 +0100

    some fixes

--- ca.hpp
+++ ca.hpp
@@ -343,11 +343,15 @@ public:
   }
 
   friend void sort(auto const b, decltype(b) e)
+    requires(std::is_same_v<iterator, std::remove_const_t<decltype(b)>> ||
+      std::is_same_v<reverse_iterator, std::remove_const_t<decltype(b)>>)
   {
     sort(b, e, std::distance(b, e), std::less<value_type>());
   }
 
   friend void sort(auto const b, decltype(b) e, auto cmp)
+    requires(std::is_same_v<iterator, std::remove_const_t<decltype(b)>> ||
+      std::is_same_v<reverse_iterator, std::remove_const_t<decltype(b)>>)
   {
     sort(b, e, std::distance(b, e), cmp);
   }
