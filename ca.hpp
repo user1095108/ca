683eebaf34628991994972afc799372e372b3e42
Janez Žemva <janezz55@gmail.com>
2021-12-20 06:14:35 +0100

    some fixes

--- ca.hpp
+++ ca.hpp
@@ -376,7 +376,7 @@ public:
     requires(std::is_same_v<iterator, std::remove_const_t<decltype(b)>> ||
       std::is_same_v<reverse_iterator, std::remove_const_t<decltype(b)>>)
   {
-    sort(b, e, std::less<value_type>());
+    sort(b, e, std::distance(b, e), std::less<value_type>());
   }
 
   friend void sort(auto const b, decltype(b) e, auto&& cmp)
