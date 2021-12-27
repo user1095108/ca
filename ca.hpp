67c578a860c493e2954da3beb436b8443d8822f0
Janez Žemva <janezz55@gmail.com>
2021-12-27 15:24:20 +0100

    some fixes

--- ca.hpp
+++ ca.hpp
@@ -24,7 +24,7 @@ public:
   using value_type = T;
 
   using difference_type = std::ptrdiff_t;
-  using size_type = std::size_t;
+  using size_type = std::ptrdiff_t;
   using reference = value_type&;
   using const_reference = value_type const&;
 
