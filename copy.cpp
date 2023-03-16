89ef9527e7620fb22e0ab68e9c399a41f32e5255
anon <>
2023-03-16 11:06:39 +0100

    some fixes

---
+++ copy.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <numeric>
+
+#include "array.hpp"
+
+int main()
+{
+  ca::array<int, 1024> s, d;
+
+  s.resize(s.capacity());
+  std::iota(s.begin(), s.end(), 0);
+
+  ca::split(
+    s.begin(),
+    s.end(),
+    [&](auto const b, decltype(b) e) noexcept
+    {
+      auto const sz0(e - b);
+      auto const l(d.last());
+
+      d.resize(d.size() + sz0);
+
+      auto const sz1(std::size_t(&d.data()[d.array_size() - 1] - l) + 1);
+
+      if (sz1 < sz0)
+      {
+        auto const df(sz0 - sz1);
+        std::copy(b, e - df, l);
+        std::copy(e - df, e, d.data());
+      }
+      else
+      {
+        std::copy(b, e, l);
+      }
+    }
+  );
+
+  std::copy(d.crbegin(), d.crend(), std::ostream_iterator<int>(std::cout, "\n"));
+
+  return 0;
+}
