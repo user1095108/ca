f2acb259fa8cb6f2932a9c9985dd5ef656b0e341
anon <>
2023-03-17 01:58:28 +0100

    some fixes

--- copy.cpp
+++ copy.cpp
@@ -3,6 +3,37 @@
 
 #include "array.hpp"
 
+template <
+  typename T1, std::size_t S1, enum ca::Method M1,
+  typename T2, std::size_t S2, enum ca::Method M2
+>
+auto copy(ca::array<T1, S1, M1> const& s, ca::array<T2, S2, M2>& d) noexcept
+{
+  std::size_t r{};
+
+  ca::split(
+    s.begin(),
+    s.end(),
+    [&](auto const b, decltype(b) e) noexcept
+    {
+      auto const sz0(std::min(std::size_t(e - b), d.capacity() - d.size()));
+      auto const l(d.last());
+      auto const sz1(std::size_t(&d.data()[d.array_size() - 1] - l) + 1);
+
+      if (d.resize(d.size() + sz0); sz1 < sz0)
+      {
+        std::copy(e - (sz0 - sz1), e, d.data());
+      }
+
+      std::copy(b, b + sz1, l);
+
+      r += sz0;
+    }
+  );
+
+  return r;
+}
+
 int main()
 {
   ca::array<int, 1024> s, d;
@@ -10,29 +41,7 @@ int main()
   s.resize(s.capacity());
   std::iota(s.begin(), s.end(), 0);
 
-  ca::split(
-    s.begin(),
-    s.end(),
-    [&](auto const b, decltype(b) e) noexcept
-    {
-      std::size_t const sz0(e - b);
-      auto const l(d.last());
-      auto const sz1(std::size_t(&d.data()[d.array_size() - 1] - l) + 1);
-      std::size_t ds;
-
-      if (d.resize(d.size() + sz0); sz1 < sz0)
-      {
-        ds = sz0 - sz1;
-        std::copy(e - ds, e, d.data());
-      }
-      else
-      {
-        ds = {};
-      }
-
-      std::copy(b, e - ds, l);
-    }
-  );
+  std::cout << copy(s, d) << std::endl;
 
   std::copy(d.crbegin(), d.crend(), std::ostream_iterator<int>(std::cout, "\n"));
 
