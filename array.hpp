b548000f13a71daa9a1bd844741f07d408ab311e
user1095108 <janezz55@gmail.com>
2023-10-12 19:21:19 +0200

    some fixes

--- array.hpp
+++ array.hpp
@@ -14,7 +14,12 @@ namespace ca
 enum Method { MEMBER, NEW, USER };
 
 template <typename T, std::size_t CAP, enum Method M = MEMBER>
-  requires ((CAP > 0) && (CAP <= PTRDIFF_MAX)) // CAP = N - 1
+  requires(
+    (CAP > 0) && (CAP <= PTRDIFF_MAX) &&
+    std::is_default_constructible_v<T> &&
+    std::is_assignable_v<T&, T const&> &&
+    std::is_assignable_v<T&, T&&>
+  ) // CAP = N - 1
 class array
 {
   friend class arrayiterator<T, array>;
