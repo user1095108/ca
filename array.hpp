283c98640fd02b35c214f5b90ce30a8ab33965df
user1095108 <janezz55@gmail.com>
2023-12-17 13:47:02 +0100

    some fixes

--- array.hpp
+++ array.hpp
@@ -703,8 +703,8 @@ constexpr auto find_if(array<T, S, M> const& c, auto pred)
 {
   auto i(c.begin()), j(c.end());
 
-  while ((i != j) && (i != --j))
-    if (pred(*i)) return i; else if (pred(*j)) return j; else ++i;
+  for (; (i != j) && (i != --j); ++i)
+    if (pred(*i)) return i; else if (pred(*j)) return j;
 
   return i && pred(*i) ? i : c.end();
 }
