7f8cdeeab1dfa5ee10caddd73ec1d5292577e548
user1095108 <janezz55@gmail.com>
2024-01-05 22:34:03 +0100

    some fixes

--- arrayiterator.hpp
+++ arrayiterator.hpp
@@ -16,7 +16,7 @@ namespace detail
 
 constexpr auto assign(auto& ...a) noexcept
 { // assign idiom
-  return [&](auto const ...b) noexcept { [](...)noexcept{}((a = b)...); };
+  return [&](auto const ...b) noexcept { assign((a = b)...); };
 }
 
 }
