63ee14da2d05436537d3ac456be0d4de1fdef5f9
user1095108 <janezz55@gmail.com>
2024-01-10 14:28:40 +0100

    some fixes

--- array.hpp
+++ array.hpp
@@ -571,14 +571,13 @@ public:
   }
 
   constexpr void append_range(std::ranges::input_range auto&& rg)
-    noexcept(noexcept(std::copy(std::ranges::begin(rg), std::ranges::end(rg),
-      std::back_inserter(*this))))
+    noexcept(noexcept(std::ranges::copy(rg, std::back_inserter(*this))))
   {
     std::ranges::copy(rg, std::back_inserter(*this));
   }
 
   constexpr void prepend_range(std::ranges::input_range auto&& rg)
-    noexcept(noexcept(std::copy(std::ranges::rbegin(rg), std::ranges::rend(rg),
+    noexcept(noexcept(std::ranges::copy(std::ranges::views::reverse(rg),
       std::front_inserter(*this))))
   {
     std::ranges::copy(std::ranges::views::reverse(rg),
